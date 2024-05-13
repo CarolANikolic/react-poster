9b7aa782989d059b6af8dba16481e555267ae3e2
Carol A. Nikolic <carolnikolic@gmail.com>
2024-05-13 15:13:56 +0200

    refactor: use useNavigate hook to navigate one path/level before when closing the modal.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,17 @@
 import styles from './Modal.module.css';
+import { useNavigate } from 'react-router-dom';
 
-const Modal = ({ children, onClose }) => {
+const Modal = ({ children }) => {
+    const navigate = useNavigate();
+    
+    function closeHandler() {
+        // Flexible way to command the navigation one level before (one path before);
+        navigate('..');
+    }
+
     return ( 
         <>
-            <div className={styles.backdrop} onClick={onClose}/>
+            <div className={styles.backdrop} onClick={closeHandler}/>
             <dialog 
                 open
                 className={styles.modal}>
